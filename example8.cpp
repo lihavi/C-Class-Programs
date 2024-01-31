0e2eae62a6a1f030b95342d2ce2ef38cbb2dbd50
vivianne <viviannjoroge91@gmail.com>
2024-01-31 13:07:33 +0300

    add c++ programs on nested if

--- example8.cpp
+++ example8.cpp
@@ -6,17 +6,26 @@ int main(){
     int num1, num2;
     cout << "Please enter two numbers" << endl;
     cin>>num1>>num2;
+  if(num1>100)
+     if (num2>100)
+        cout<<"great"<<endl;
+            else
+            cout<<"check only num1"<<endl;
+    
+        else
+        cout<<"not ok"<<endl;
 
-    int product = num1*num2;
+
+    /* nt product = num1*num2;
     int division = num1/num2;
     int subtraction = num1-num2;
-    int modulus = num1%num2;
+    int modulus = num1%num2; */
 
-    cout << "product is " << product << endl;
+   /*  cout << "product is " << product << endl;
     cout << "divison is " << division << endl;
     cout << "subtraction is " << subtraction << endl;
     cout << "modulus is " << modulus << endl;
-
+ */
 
     return 0;
 
